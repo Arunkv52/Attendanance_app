1e9a638332147843331ce7d909d26051fd012e38
Arunkv52 <arunkbva5@gmail.com>
2026-01-08 17:54:42 +0530

    files add

--- src/pages/users/User.tsx
+++ src/pages/users/User.tsx
@@ -1,8 +1,31 @@
+import Header from '@/layout/Header'
+import Sidebar from '@/layout/Sidebar'
+import { ThemeContext } from '../../context/ThemeContext'
+import { useContext } from 'react'
+
 
 const User = () => {
+    const { theme }: any = useContext(ThemeContext)
+  
   return (
-    <div>User</div>
+     <>
+      <main
+        className={`${
+          theme === 'light' ? 'bg-white text-black' : 'bg-black text-white'
+        } mx-0 flex justify-start items-start gap-2`}
+      >
+        <Sidebar />
+        <div className='main-layout w-full ml-15'>
+          <Header />
+          <div className='px-3 mt-5'>
+            <h1 className='text-2xl py-5'>📅 List of Users</h1>
+            
+          </div>
+          
+        </div>
+      </main>
+    </>
   )
-};
+}
 
-export default User;
+export default User
